bcfebee270c8f341b7ec5538631f83b194cdbcb3
Williston Hayes <wshayes@gmail.com>
2012-07-14 12:00:35 +0200

    Added shell script for easy sym linking of dotfiles

---
+++ set_links.sh
@@ -0,0 +1,7 @@
+ln -si ~/git/dotfiles/.vimrc ~/
+ln -si ~/git/dotfiles/.vim ~/
+ln -si ~/git/dotfiles/.gitignore ~/
+ln -si ~/git/dotfiles/.gitconfig ~/
+ln -si ~/git/dotfiles/.bashrc ~/
+ln -si ~/git/dotfiles/.bash_profile ~/
+ln -si ~/git/dotfiles/.tmux.conf ~/
